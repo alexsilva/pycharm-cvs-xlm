88d7dec22207577513aef18525c0cce2c969b6be
alex <geniofuturo@gmail.com>
2017-12-22 18:42:06 -0200

    setup to install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pychnarm-vcs-xml',
+    py_modules=['gitpycharm'],
+    version='1.0',
+    url='https://github.com/alexsilva/pycharm-cvs-xlm',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Search for git submodules in the project and add them to the vcs.xml file from a pycharm (.idea) configuration directory.'
+)
