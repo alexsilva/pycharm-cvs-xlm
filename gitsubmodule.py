7edc133053e5a6666477e6121c97bbd5d6d3a9bf
alex <geniofuturo@gmail.com>
2018-02-23 16:47:38 -0300

    Auxiliary script for working with submodules.

---
+++ gitsubmodule.py
@@ -0,0 +1,20 @@
+import sys
+
+from gitpycharm import Project
+
+"""
+Updates submodules of a project
+"""
+
+if __name__ == "__main__":
+    projectdir = sys.argv[-1]
+
+    print "Project update \"{}\"".format(projectdir)
+    project = Project(projectdir)
+    project.update()
+
+    print "Loading submodules..."
+    project.load_submodules()
+
+    for submodule in project:
+        submodule.update()
